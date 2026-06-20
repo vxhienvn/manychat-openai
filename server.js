1d6a72725818bfc1e2c479392310a4b7b350f1c4
vxhienvn <82212518+vxhienvn@users.noreply.github.com>
2026-06-20 20:45:57 +0000

    messenger auto reply

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const app = express();
 
 app.use(express.json());
 
-const VERIFY_TOKEN = "Vietnam84@";
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 app.get('/', (req, res) => {
     res.send('Server OK');
 });
 
 app.get('/webhook', (req, res) => {
+
     const mode = req.query['hub.mode'];
     const token = req.query['hub.verify_token'];
     const challenge = req.query['hub.challenge'];
@@ -23,17 +25,45 @@ app.get('/webhook', (req, res) => {
     }
 });
 
-app.post('/webhook', (req, res) => {
-    console.log(JSON.stringify(req.body, null, 2));
-    res.sendStatus(200);
-});
+app.post('/webhook', async (req, res) => {
 
-app.post('/chat', (req, res) => {
-    console.log(req.body);
+    const body = req.body;
 
-    res.json({
-        reply: 'Xin chào từ Render'
-    });
+    if (body.object === 'page') {
+
+        for (const entry of body.entry) {
+
+            const webhookEvent = entry.messaging[0];
+
+            if (!webhookEvent.message) continue;
+
+            const senderId = webhookEvent.sender.id;
+
+            await fetch(
+                `https://graph.facebook.com/v23.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`,
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        recipient: {
+                            id: senderId
+                        },
+                        message: {
+                            text: 'Chào anh/chị. Em là trợ lý AI của Tổng Kho Thiết Bị Bếp & Nhà Tắm Miền Bắc. Anh/chị đang quan tâm quạt hay thiết bị vệ sinh ạ?'
+                        }
+                    })
+                }
+            );
+
+        }
+
+        res.status(200).send('EVENT_RECEIVED');
+        return;
+    }
+
+    res.sendStatus(404);
 });
 
 const PORT = process.env.PORT || 10000;
