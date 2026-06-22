be29e1708725dc46a1470b73d8e70dd99004cb99
vxhienvn <82212518+vxhienvn@users.noreply.github.com>
2026-06-22 16:37:32 +0000

    add faucet carousel

--- server.js
+++ server.js
@@ -60,25 +60,39 @@ function detectProductType(customerMessage, historyText) {
     const msg = customerMessage.toLowerCase();
     const history = historyText.toLowerCase();
 
+    const faucetWords = [
+        "lavabo", "chậu lavabo", "chau lavabo",
+        "sen", "sen tắm", "sen tam",
+        "vòi", "voi", "vòi rửa", "voi rua",
+        "chậu rửa", "chau rua"
+    ];
+
     const bathWords = [
         "combo", "phòng tắm", "phong tam", "nhà tắm", "nha tam",
         "nhà vệ sinh", "nha ve sinh", "thiết bị vệ sinh", "thiet bi ve sinh",
-        "bồn cầu", "bon cau", "lavabo", "sen tắm", "sen tam",
-        "bồn tắm", "bon tam", "bếp", "bep", "gạch", "gach"
+        "bồn cầu", "bon cau", "bồn tắm", "bon tam", "bếp", "bep", "gạch", "gach"
     ];
 
     const fanWords = [
         "quạt", "quat", "quạt trần", "quat tran", "quạt đèn", "quat den",
-        "5 cánh", "8 cánh", "10 cánh", "55w", "65w", "70w", "90w"
+        "5 cánh", "8 cánh", "10 cánh", "55w", "65w", "70w", "90w", "cho xem quạt", "xem quạt", "gửi quạt", "gui quat", "xin quạt", "xin quat"
     ];
 
+    if (faucetWords.some(word => msg.includes(word))) return "faucet";
     if (bathWords.some(word => msg.includes(word))) return "combo";
     if (fanWords.some(word => msg.includes(word))) return "fan";
 
-    const askImageWords = ["gửi ảnh","xem", "xin ảnh", "xin anh", "gui anh", "xem mẫu", "xem mau", "cho xem", "gửi mẫu", "gui mau", "mẫu", "mau", "ảnh"];
+    const askImageWords = [
+        "gửi ảnh", "gui anh", "xin ảnh", "xin anh",
+        "xem ảnh", "xem anh", "cho ảnh", "cho anh",
+        "xem mẫu", "xem mau", "cho xem", "gửi mẫu", "gui mau",
+        "xin mẫu", "xin mau", "cho mẫu", "cho mau", "xem"
+    ];
+
     const isAskingImage = askImageWords.some(word => msg.includes(word));
 
     if (isAskingImage) {
+        if (faucetWords.some(word => history.includes(word))) return "faucet";
         if (bathWords.some(word => history.includes(word))) return "combo";
         if (fanWords.some(word => history.includes(word))) return "fan";
     }
@@ -88,15 +102,18 @@ function detectProductType(customerMessage, historyText) {
 
 function shouldSendCarousel(customerMessage) {
     const msg = customerMessage.toLowerCase();
-   const words = [
-    "gửi ảnh", "gui anh",
-    "xin ảnh", "xin anh",
-    "xem ảnh", "xem anh",
-    "cho ảnh", "cho anh",
-    "xem mẫu", "xem mau",
-    "cho xem", "gửi mẫu", "gui mau",
-"xin mẫu", "xin mau","cho mẫu", "cho mau", "xem", "xem combo", "xem nhà tắm", ""
-];
+
+    const words = [
+        "gửi ảnh", "gui anh",
+        "xin ảnh", "xin anh",
+        "xem ảnh", "xem anh",
+        "cho ảnh", "cho anh",
+        "xem mẫu", "xem mau",
+        "cho xem", "gửi mẫu", "gui mau", "gửi ảnh", "gui anh", "ảnh", "anh",
+        "xin mẫu", "xin mau",
+        "cho mẫu", "cho mau"
+    ];
+
     return words.some(word => msg.includes(word));
 }
 
@@ -150,6 +167,7 @@ QUY TẮC:
 - Không tư vấn sâu khi khách chưa cho số điện thoại hoặc zalo, chỉ cần khách trả lời thông tin tư vấn 2 hoặc 3 câu là xin số điện thoại/Zalo để tư vấn chi tiết hơn.
 - Nếu khách nhắn tin ký tự khó hiểu, không dịch được hoặc phàn nàn chất lượng ảnh, video, thông tin thì không cần xin lỗi và hỏi lại vấn đề gì hay cần hỗ trợ gì, xin số điện thoại/zalo gọi điện trực tiếp cho tiện
 - Tối đa 4 câu, tối đa 80 từ.
+- Quy tắc sau khi gửi ảnh, video hoặc slide đều phải nói thêm rằng đây là một số mẫu bán chạy để khách tham khảo, còn rất nhiều mẫu nữa, do hạn chế tin nhắn Quảng cáo chỉ gửi được vậy, nếu muốn xem thêm, tư vấn chi tiết hơn hoặc xem thêm mẫu thì có thể liên hệ qua Zalo hoặc gọi điện để được hỗ trợ tốt nhất, có thể xin sdt zalo để tư vấn và gửi thêm luôn,tránh tình trạng khách chỉ xem ảnh rồi không phản hồi lại nữa.
 - Luôn kết thúc bằng câu hỏi tự nhiên.
 
 LỊCH SỬ HỘI THOẠI:
@@ -280,6 +298,18 @@ async function sendComboCarousel(senderId) {
 async function sendFanCarousel(senderId) {
     const elements = [
         {
+            title: "Quạt 10 cánh cao cấp",
+            subtitle: "Sải lớn, hợp phòng khách rộng, không gian sang trọng",
+            image_url: "https://scontent.fhan5-9.fna.fbcdn.net/v/t45.1600-4/728597413_3412225568953938_5048258706912707012_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=d5bd00&_nc_eui2=AeF9_KEAt19bMbLGn9ImPdBErXek8XEmc1Std6TxcSZzVJcqNZD2S29UtKFH2hKEKAanUmzGmvpFHDAbbuFUebxx&_nc_ohc=9hQEkg60bncQ7kNvwFMP4Qb&_nc_oc=AdoO5dx259kvQ_3xJWioFcjyyCEHM9XD2jwHQ5Jn2d78H8ZBjY6JwcRy6QbFFIm6P8E&_nc_zt=1&_nc_ht=scontent.fhan5-9.fna&_nc_gid=H81qwU0PpFnPWUSKJeZqCw&_nc_ss=7b2a8&oh=00_Af8j0NRqieKJFAi1UyLA5JHDTbH_cX8-3a8q1Oi9S-uAiw&oe=6A3F1338",
+            buttons: [{ type: "phone_number", title: "Gọi tư vấn", payload: "0973693677" }]
+        },
+        {
+            title: "Quạt 10-8 cánh mẫu 2",
+            subtitle: "Mẫu trang trí cao cấp, hợp phòng khách lớn",
+            image_url: "https://scontent.fhan5-2.fna.fbcdn.net/v/t45.1600-4/728618331_3412225595620602_3289339737406152436_n.png?stp=dst-jpg_tt6&_nc_cat=102&ccb=1-7&_nc_sid=d5bd00&_nc_eui2=AeFD7mMN7hdaPIBQRcOohMdLoNSH175SZbug1IfXvlJlu0TcfGdylmrTyaVWAlebn5lHCAp5ciKNuqaTybcsSeMz&_nc_ohc=a8qSkZXNdqAQ7kNvwF3qIGb&_nc_oc=Ado36HTGSvmBt3zfu0au8OoN79CtIHo0NYkxNr0p8pDLfxP65QY-FEGXXQDy-cs5gIo&_nc_zt=1&_nc_ht=scontent.fhan5-2.fna&_nc_gid=AhyAU3j8PpSeWDT8Z0Gu_Q&_nc_ss=7b2a8&oh=00_Af_IRHEjLdKzq57l6YVQHLBb5q2zsY_CHAOLoMIvYEr_Jw&oe=6A3EF058",
+            buttons: [{ type: "phone_number", title: "Gọi tư vấn", payload: "0973693677" }]
+        },
+        {
             title: "Quạt trần 5 cánh 55W",
             subtitle: "Phù hợp phòng vừa, mẫu hiện đại, dễ phối nội thất",
             image_url: "https://scontent.fhan5-2.fna.fbcdn.net/v/t45.1600-4/727719223_3412214488955046_3127207876950040699_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=d5bd00&_nc_eui2=AeGIbhNqPizCG827jRKi2ujsHQ5r-eC51B0dDmv54LnUHXQPXRcQPF7TG8HJicZfMBYw702DbO6KpFWzJH9aJm5T&_nc_ohc=V-ExMLpYn9EQ7kNvwGsOcGj&_nc_oc=Adr5zBbqRVUNFvx2QhS0oYraja93d0EFWSWxPusfqiE-r3ppgR8l4wSWdCKpgCnaf24&_nc_zt=1&_nc_ht=scontent.fhan5-2.fna&_nc_gid=bcw2J8GkfUEXJriVz1oLOQ&_nc_ss=7b2a8&oh=00_Af_YhRRmZgntjGTgS36kmpcsqaU1W_kyjRzgDCs2mVLdwA&oe=6A3F095A",
@@ -308,22 +338,72 @@ async function sendFanCarousel(senderId) {
             subtitle: "Tông nâu gỗ, hợp nội thất ấm và sang",
             image_url: "https://scontent.fhan5-6.fna.fbcdn.net/v/t45.1600-4/728532874_3412214428955052_5606934162449542415_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=d5bd00&_nc_eui2=AeFDdkyqaqMM5-Od42GYHs1oha648laqL8GFrrjyVqovwZ2K2yEM2sVGjDIE9ihvZoJTeuwlmp9cPpJdEp5Ev1_r&_nc_ohc=heosDSt4O5YQ7kNvwHxIwm6&_nc_oc=Adq3bKbZRsGQjOa-04Xbl4O6r_o6yph7GM4g3s95kZ29XVNJZQyek2W_6n8hTfplruk&_nc_zt=1&_nc_ht=scontent.fhan5-6.fna&_nc_gid=XVJP-C1yMwqqDktU5JceRQ&_nc_ss=7b2a8&oh=00_Af-L4TE1d8ByLAiXF2HLYsFEbSYpkjVPxWHNu7MNadBoTg&oe=6A3F13B0",
             buttons: [{ type: "phone_number", title: "Gọi tư vấn", payload: "0973693677" }]
-        },
+        }
+        
+    ];
+
+    await sendTemplate(senderId, elements, "Fan carousel");
+}
+
+async function sendFaucetCarousel(senderId) {
+    const elements = [
         {
-            title: "Quạt 10 cánh cao cấp",
-            subtitle: "Sải lớn, hợp phòng khách rộng, không gian sang trọng",
-            image_url: "https://scontent.fhan5-9.fna.fbcdn.net/v/t45.1600-4/728597413_3412225568953938_5048258706912707012_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=d5bd00&_nc_eui2=AeF9_KEAt19bMbLGn9ImPdBErXek8XEmc1Std6TxcSZzVJcqNZD2S29UtKFH2hKEKAanUmzGmvpFHDAbbuFUebxx&_nc_ohc=9hQEkg60bncQ7kNvwFMP4Qb&_nc_oc=AdoO5dx259kvQ_3xJWioFcjyyCEHM9XD2jwHQ5Jn2d78H8ZBjY6JwcRy6QbFFIm6P8E&_nc_zt=1&_nc_ht=scontent.fhan5-9.fna&_nc_gid=H81qwU0PpFnPWUSKJeZqCw&_nc_ss=7b2a8&oh=00_Af8j0NRqieKJFAi1UyLA5JHDTbH_cX8-3a8q1Oi9S-uAiw&oe=6A3F1338",
+            title: "Sen tắm, vòi, chậu rửa 01",
+            subtitle: "Mẫu thiết bị vệ sinh đẹp, phù hợp nhà tắm hiện đại",
+            image_url: "https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-6/703191027_969113915979098_8030725390918618210_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFa7VMnu1zyij_pDvCcZvU6CxM-YlbmHdwLEz5iVuYd3Ph9NGGH5Lt0ikG62Y373ByO0bTV7AvLIVbz5YfYWkPz&_nc_ohc=FXIstFkz2jgQ7kNvwGR7x46&_nc_oc=Adp2CFxuAzGCYDM_R3v28Nvu4YLJ6NGsOtsQCxTJB2ksW8HX5Fgh5IC0SBWYWuk_Xuc&_nc_zt=23&_nc_ht=scontent.fhan5-10.fna&_nc_gid=lw2ZNwyTAGhLXWF-rtdkWA&_nc_ss=7b2a8&oh=00_Af_EgBdj7r_WvXERm2fgzd05vy75frJVDtiEqjKEltcRug&oe=6A3F3267",
             buttons: [{ type: "phone_number", title: "Gọi tư vấn", payload: "0973693677" }]
         },
         {
-            title: "Quạt 10-8 cánh mẫu 2",
-            subtitle: "Mẫu trang trí cao cấp, hợp phòng khách lớn",
-            image_url: "https://scontent.fhan5-2.fna.fbcdn.net/v/t45.1600-4/728618331_3412225595620602_3289339737406152436_n.png?stp=dst-jpg_tt6&_nc_cat=102&ccb=1-7&_nc_sid=d5bd00&_nc_eui2=AeFD7mMN7hdaPIBQRcOohMdLoNSH175SZbug1IfXvlJlu0TcfGdylmrTyaVWAlebn5lHCAp5ciKNuqaTybcsSeMz&_nc_ohc=a8qSkZXNdqAQ7kNvwF3qIGb&_nc_oc=Ado36HTGSvmBt3zfu0au8OoN79CtIHo0NYkxNr0p8pDLfxP65QY-FEGXXQDy-cs5gIo&_nc_zt=1&_nc_ht=scontent.fhan5-2.fna&_nc_gid=AhyAU3j8PpSeWDT8Z0Gu_Q&_nc_ss=7b2a8&oh=00_Af_IRHEjLdKzq57l6YVQHLBb5q2zsY_CHAOLoMIvYEr_Jw&oe=6A3EF058",
+            title: "Sen tắm, vòi, chậu rửa 02",
+            subtitle: "Phù hợp combo nhà tắm, lavabo, sen vòi đồng bộ",
+            image_url: "https://scontent.fhan5-8.fna.fbcdn.net/v/t39.30808-6/703434110_969113955979094_6569529519103445901_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeHH32-t5cp1HOEwtLnl4efgQhU--7nx89lCFT77ufHz2QV1yYoahgSVjwQI2-zeUAnF19mFaOZxkGYq-bgw-2hd&_nc_ohc=j9lhKFp23ucQ7kNvwHvYQhG&_nc_oc=AdpuHFpaeWpEMrFjRKfm3UV9cWafoW778nyCdhmjr4fL6UaY6WC9_OOHLGGyvgZDyYs&_nc_zt=23&_nc_ht=scontent.fhan5-8.fna&_nc_gid=2nlcgGcyzzW8UV4MhdfXGQ&_nc_ss=7b2a8&oh=00_Af8RbP_8DlRACiv4mKMStD_AoNEj9jbywVqODZk3agHO4A&oe=6A3F4C11",
+            buttons: [{ type: "phone_number", title: "Gọi tư vấn", payload: "0973693677" }]
+        },
+        {
+            title: "Sen tắm, vòi, chậu rửa 03",
+            subtitle: "Mẫu dễ phối cho phòng tắm mới hoặc cải tạo",
+            image_url: "https://scontent.fhan5-8.fna.fbcdn.net/v/t39.30808-6/706144052_969111872645969_6005742897087813212_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGj-42sFoINEVRGYCMM1R_UKp_Y-w156xIqn9j7DXnrEmKdcMcFHXS3zI-Y2TMt6tPxyur0gAGB4wBddvWthwcn&_nc_ohc=E9kNIqp-bZQQ7kNvwEw5yxd&_nc_oc=AdqkXpglB2t7rhpnWZVBceSRhmbOumuWVt9Oo7iVGB8p3dF8Gnyy-hXCjSv3HxrNRAo&_nc_zt=23&_nc_ht=scontent.fhan5-8.fna&_nc_gid=lw2ZNwyTAGhLXWF-rtdkWA&_nc_ss=7b2a8&oh=00_Af-lY4oCbYzy5GDorhoCMILonvaSzNy-aa7bQt7-RGSX0Q&oe=6A3F4915",
+            buttons: [{ type: "phone_number", title: "Gọi tư vấn", payload: "0973693677" }]
+        },
+        {
+            title: "Sen tắm, vòi, chậu rửa 04",
+            subtitle: "Thiết bị vệ sinh đẹp, dùng cho phòng tắm gia đình",
+            image_url: "https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-6/703762770_969115259312297_1234080936578252503_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeHE_8YJMCLPCcQYgjumiKeTgWMGOUdcXKqBYwY5R1xcquANKOQ9Uf9pMxiS4QBdADat6q0Q8SCuY6WSXdTEmm_E&_nc_ohc=ODXv-MU4eSUQ7kNvwG7uIeC&_nc_oc=AdrNUwwyDwfX_v5xCCIYW--5d5QfNzzRQB2o4hR1ZlQbS6R5srP_78Ej1zHwmfq3UVs&_nc_zt=23&_nc_ht=scontent.fhan5-10.fna&_nc_gid=2nlcgGcyzzW8UV4MhdfXGQ&_nc_ss=7b2a8&oh=00_Af_gojJu4NmULdT9o_hrC4s_jKuapV2mcdWKdqyKhgG4gA&oe=6A3F4185",
+            buttons: [{ type: "phone_number", title: "Gọi tư vấn", payload: "0973693677" }]
+        },
+        {
+            title: "Sen tắm, vòi, chậu rửa 05",
+            subtitle: "Nhiều mẫu sen vòi, chậu rửa, phụ kiện nhà tắm",
+            image_url: "https://scontent.fhan5-11.fna.fbcdn.net/v/t39.30808-6/703702032_969117399312083_6892088813610533309_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEvTWzHrIEhrIerdEc_k2iDSE2UrPzOhPFITZSs_M6E8Sk6makKCWFrEoHCr7oDfEXJvDVne13v71uXRSrzRPzj&_nc_ohc=mHj5B7Z9BL4Q7kNvwFelrTN&_nc_oc=Ados1qvWrG3HRD1sbVz3-A2XILgJkP5dmiIEJepme3MVglSvpEBAygL6eufk1lPoKws&_nc_zt=23&_nc_ht=scontent.fhan5-11.fna&_nc_gid=x8iKRicU2knMbmoxx1AlrQ&_nc_ss=7b2a8&oh=00_Af_hmtEdk1jG6IwfcGnWip7TsMXILlPdw0_EgIN5N6J3Tg&oe=6A3F202F",
+            buttons: [{ type: "phone_number", title: "Gọi tư vấn", payload: "0973693677" }]
+        },
+        {
+            title: "Sen tắm, vòi, chậu rửa 06",
+            subtitle: "Phù hợp chọn lẻ hoặc phối thành combo phòng tắm",
+            image_url: "https://scontent.fhan5-11.fna.fbcdn.net/v/t39.30808-6/703394730_969102089313614_7563853999344157562_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGvzNma-qzwKRgUIPHbS059IIw15_LYPAUgjDXn8tg8BV2Ut5nwfaeG5lp_YcacQ3Cr3QwwqDKWLc2LX4K74PzG&_nc_ohc=nmQI8xCRMDEQ7kNvwFXJeCi&_nc_oc=AdoYM8KTvfsgjVWIvqQssnfYBm1hlQrHLsK7fXayjDETX17KsiemOzMWBvsvc149PhA&_nc_zt=23&_nc_ht=scontent.fhan5-11.fna&_nc_gid=bhKe68cscEfcIRc6L2-GAw&_nc_ss=7b2a8&oh=00_Af_76vjM5pypwxqSK04aZBdo6_WdM4oOIcw5lqo7zOlg4A&oe=6A3F3660",
+            buttons: [{ type: "phone_number", title: "Gọi tư vấn", payload: "0973693677" }]
+        },
+        {
+            title: "Sen tắm, vòi, chậu rửa 07",
+            subtitle: "Dòng sen vòi, chậu rửa đẹp cho phòng tắm hiện đại",
+            image_url: "https://scontent.fhan5-8.fna.fbcdn.net/v/t39.30808-6/697199723_962937126596777_8498925046413907390_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeGlX_8xtvPrub2HpN7Hzfku3fOs0Y5yUxDd86zRjnJTELZEtl4LzdKXfdSiqOoRhJ6tWcQ5P4A1etQYs76IPG63&_nc_ohc=v8W4ap3J-R8Q7kNvwE-cwga&_nc_oc=AdonMkXkkUYTM8qvwpDFjqEoJpil0dP62pjuOl75hbfslugIhe8k_pH0c4AgHcqtRQM&_nc_zt=23&_nc_ht=scontent.fhan5-8.fna&_nc_gid=94SF_MxkkrVw54j0iFlASQ&_nc_ss=7b2a8&oh=00_Af-8-ZdfJDnjjDo9_6HmC06n7lXN8lhknfBGK5jpoDPLEw&oe=6A3F2B00",
+            buttons: [{ type: "phone_number", title: "Gọi tư vấn", payload: "0973693677" }]
+        },
+        {
+            title: "Sen tắm, vòi, chậu rửa 08",
+            subtitle: "Mẫu phù hợp khách muốn chọn thiết bị đồng bộ",
+            image_url: "https://scontent.fhan5-11.fna.fbcdn.net/v/t39.30808-6/698445527_962937943263362_5571045644058564871_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeFgcEY2yasUofU8BdNvLvpalsJuq_DOb4-Wwm6r8M5vj6smUIYHJoT_QSDAurTLkIRIGdMSZVqZj4w_xjbGV5Qg&_nc_ohc=bQ4bkAprWWcQ7kNvwFLqrJk&_nc_oc=AdqteoGgcC4ZC2eWLRsKE3P01FfgwUE2_YLSBtCc8JrwsK-w7BzBXHsswoeDZ_KhSH4&_nc_zt=23&_nc_ht=scontent.fhan5-11.fna&_nc_gid=5yF49pHm5e0YEaFaAKWDgg&_nc_ss=7b2a8&oh=00_Af8juSDpxk4_p1GkD8RDzR2dh1dSTfiyd78qC_1bH7O_YQ&oe=6A3F2C29",
+            buttons: [{ type: "phone_number", title: "Gọi tư vấn", payload: "0973693677" }]
+        },
+        {
+            title: "Sen tắm, vòi, chậu rửa 09",
+            subtitle: "Có thể phối cùng lavabo, bồn cầu, tủ chậu",
+            image_url: "https://scontent.fhan5-2.fna.fbcdn.net/v/t39.30808-6/698948687_962937056596784_1757048518063761369_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeGL5Q10OoGXwxb9VEsfUkDBJg4_NdA8AuImDj810DwC4qnm3OY1AxUv9vaIlWs1oFtSmNKdu2hT2-aSCKW3fv7l&_nc_ohc=FasGTiiRATQQ7kNvwG3EAM5&_nc_oc=Adp5QKem01g3WYOWmHru10TtFFt_wU5NfoBlY5iI27WU6WTIM9tc7TGJ-dmHMApl_u8&_nc_zt=23&_nc_ht=scontent.fhan5-2.fna&_nc_gid=94SF_MxkkrVw54j0iFlASQ&_nc_ss=7b2a8&oh=00_Af8nmGSdK6ZHQVMBUnrklY2vyPlHe2-cVDE9wL3eLofmCg&oe=6A3F42D5",
             buttons: [{ type: "phone_number", title: "Gọi tư vấn", payload: "0973693677" }]
         }
     ];
 
-    await sendTemplate(senderId, elements, "Fan carousel");
+    await sendTemplate(senderId, elements, "Faucet carousel");
 }
 
 async function handleMessage(event) {
@@ -371,6 +451,8 @@ async function handleMessage(event) {
             await sendComboCarousel(senderId);
         } else if (productType === "fan") {
             await sendFanCarousel(senderId);
+        } else if (productType === "faucet") {
+            await sendFaucetCarousel(senderId);
         }
     }
 }
