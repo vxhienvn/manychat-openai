72a708f1a4952f17ec9420f6b94e5b647a09c9b9
vxhienvn <82212518+vxhienvn@users.noreply.github.com>
2026-07-01 03:02:12 +0700

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const { app, startBackgroundJobs } = require('./src/app');
-
-const PORT = process.env.PORT || 10000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
-    startBackgroundJobs();
-});
